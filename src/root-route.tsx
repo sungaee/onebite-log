0bd548926813b0872d5180eb092892e049526b18
sungaee <sungaee121@naver.com>
2026-04-27 15:32:55 +0900

    글로벌 레이아웃 설정

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -6,18 +6,23 @@ import IndexPage from "./pages/index-page";
 import PostDetailPage from "./pages/post-detail-page";
 import ProfileDetailPage from "./pages/profile-detail-page";
 import ResetPasswordPage from "./pages/reset-password-page";
+import GlobalLayout from "./components/ui/layout/globla-layout";
 
-export default function RootRoute(){
-    return <Routes>
-        <Route path="/sign-in" element={<SignInPage/>} />
-        <Route path="/sign-up" element={<SignUpPage/>} />
-        <Route path="/forget-password" element={<ForgetPasswordPage/>} />
+export default function RootRoute() {
+  return (
+    <Routes>
+      <Route element={<GlobalLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/forget-password" element={<ForgetPasswordPage />} />
 
-        <Route path="/" element={<IndexPage/>} />
-        <Route path="/post/:postId" element={<PostDetailPage/>} />
-        <Route path="/profile/:userId" element={<ProfileDetailPage/>} />
-        <Route path="/reset-password" element={<ResetPasswordPage/>} />
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/post/:postId" element={<PostDetailPage />} />
+        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-        <Route path="*" element={<Navigate to={'/'} />} />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Route>
     </Routes>
+  );
 }
