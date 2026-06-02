4ead1dc31cdaeb0fcc853214733242e84642da3c
sungaee <sungaee121@naver.com>
2026-06-02 15:48:56 +0900

    로그인 구현하기

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -7,11 +7,27 @@ export async function signUp({
   email: string;
   password: string;
 }) {
-  const {data, error} = await supabase.auth.signUp({
+  const { data, error } = await supabase.auth.signUp({
     email,
     password,
   });
 
-  if(error) throw error;
+  if (error) throw error;
+  return data;
+}
+
+export async function signInWithPassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw error;
   return data;
 }
