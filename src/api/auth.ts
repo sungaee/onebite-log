cdd9db015eb57c3fa6f4d2ebbec50362abdee892
sungaee <sungaee121@naver.com>
2026-06-03 19:48:36 +0900

    소셜 로그인 구현하기 (github, kakao) 진행중

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,5 @@
 import supabase from "@/lib/supabase";
+import type { Provider } from "@supabase/supabase-js";
 
 export async function signUp({
   email,
@@ -28,6 +29,14 @@ export async function signInWithPassword({
     password,
   });
 
+  if (error) throw error;
+  return data;
+}
+
+export async function signInWithOAuth(provider: Provider) {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider,
+  });
   if (error) throw error;
   return data;
 }
--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -3,12 +3,15 @@ import { Input } from "@/components/ui/input";
 import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in-with-password";
 import { useState } from "react";
 import { Link } from "react-router";
+import gitHubLogo from "@/assets/github-mark.svg";
+import { useSignInWithOAuth } from "@/hooks/mutations/use-sign-in-sith-oauth";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { mutate: signInWithPassword } = useSignInWithPassword();
+  const { mutate: signInWithOAuth } = useSignInWithOAuth();
 
   const handleSignInWithPasswordClick = () => {
     if (email.trim() === "") return;
@@ -18,6 +21,14 @@ export default function SignInPage() {
       email,
       password,
     });
+  };
+
+  const handelSignInWithGitHubClick = () => {
+    signInWithOAuth("github");
+  };
+
+  const handelSignInWithKakaoClick = () => {
+    signInWithOAuth("kakao");
   };
 
   return (
@@ -39,9 +50,24 @@ export default function SignInPage() {
           placeholder="password"
         />
       </div>
-      <div>
+      <div className="flex flex-col gap-2">
         <Button onClick={handleSignInWithPasswordClick} className="w-full">
           로그인
+        </Button>
+        <Button
+          onClick={handelSignInWithGitHubClick}
+          className="w-full"
+          variant={"outline"}
+        >
+          <img src={gitHubLogo} className="h-4 w-4" />
+          GitHub 계정으로 로그인
+        </Button>
+        <Button
+          onClick={handelSignInWithKakaoClick}
+          className="w-full"
+          variant={"outline"}
+        >
+          KaKao 계정으로 로그인
         </Button>
       </div>
       <div>
