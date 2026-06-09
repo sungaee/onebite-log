71f3cf99f4f4c2db66331b6e0ea7a6f73c8bbeac
sungaee <sungaee121@naver.com>
2026-06-09 15:05:53 +0900

    인증 에러 처리하기2( 토스트메세지로 설정한 문구 노출, 로딩 시 버튼비활성화 처리)

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -22,7 +22,7 @@ export async function signInWithPassword({
   password,
 }: {
   email: string;
-  password: string;
+  password: string; 
 }) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
